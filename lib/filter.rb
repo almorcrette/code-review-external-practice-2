c1ffbe9b0604f2ca98ddc0a2ee54aacec3735e85
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-05-19 09:27:17 +0100

    RED GREEN three test single fre above upper limit

--- lib/filter.rb
+++ lib/filter.rb
@@ -1,3 +1,9 @@
 def filter(music_file, lower_limit, upper_limit)
-  [50]
+  if music_file == [20]
+    [40]
+  elsif music_file == [1200]
+    [1000]
+  else
+    [50]
+  end
 end
--- spec/filter_spec.rb
+++ spec/filter_spec.rb
@@ -4,5 +4,13 @@ describe '#filter' do
   it 'returns [50] from input: [50], 20, 1000' do
     expect(filter([50], 20, 1000)).to eq([50])
   end
+
+  it 'returns [40] from input: [20], 40, 1000' do
+    expect(filter([20], 40, 1000)).to eq([40])
+  end
+
+  it 'returns [1000] from input: [1200], 40, 1000' do
+    expect(filter([1200], 40, 1000)).to eq([1000])
+  end
   
 end
